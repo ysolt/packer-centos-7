6543b98595a888d6a4d709408bdc18ff9ee0e6da
Zsolt Molnar <zsolt.molnar@ruckuswireless.com>
2016-11-16 22:14:46 +0100

    fix: second volume preparation

--- scripts/prepare_data_volume.sh
+++ scripts/prepare_data_volume.sh
@@ -1,3 +1,6 @@
-mkfs.ext4 -F /dev/sdc
+#!/bin/bash
+set -xe
+
+mkfs.ext4 -F /dev/sdb
 mkdir /data
-echo '/dev/sdc          /data   ext4    defaults,noexec,nosuid,nodev,nofail     0 2'>>/etc/fstab
+echo '/dev/sdb          /data   ext4    defaults,noexec,nosuid,nodev,nofail     0 2'>>/etc/fstab
