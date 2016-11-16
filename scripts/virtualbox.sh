a51be1ceb87d2002e57e82971f5267624ae74db7
Zsolt Molnar <ysolt@ysolt.net>
2016-11-16 16:36:43 +0100

    typo fixes + add stop upon error

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+set -xe
+
 yum install -y bzip2 gcc kernel-devel kernel-headers dkms nfs-utils biosdevname
 
-mount -o loop /home/lbs/VBoxGuestAdditions.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf VBoxGuestAdditions.iso
